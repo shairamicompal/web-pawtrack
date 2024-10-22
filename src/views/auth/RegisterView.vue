026a8edc11228673149332bc721039bb08456409
Shaira Micompal <shairamicompal@gmail.com>
2024-10-22 20:19:29 +0800

    fix: design; adjust logo; attached picture

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -12,7 +12,14 @@ const { mobile } = useDisplay()
       <v-container fluid>
         <v-row>
           <!-- Left Column for Desktop (hidden on mobile) -->
-          <v-col cols="12" lg="8" class="bg-surface-light h-screen" v-if="!mobile"></v-col>
+          <v-col cols="12" lg="8" class="bg-surface-light h-screen" v-if="!mobile">
+            <v-img
+              src="/images/banner-pawtrack.png"
+              class="fill-height"
+              alt="Background Image"
+              cover
+            ></v-img>
+          </v-col>
 
           <!-- Right Column for Login Form -->
           <v-col cols="12" lg="4" :class="mobile ? '' : 'pt-16'">
@@ -20,21 +27,20 @@ const { mobile } = useDisplay()
               <!-- Card Title with Logo -->
               <v-card-title class="text-center">
                 <v-img
-                  class="mx-auto"
-                  src="/images/pawt-favicon.png"
-                  :width="mobile ? '75%' : '40%'"
+                  class="mx-auto mb-10"
+                  src="/images/pawtrack-logo.png"
+                  :width="mobile ? '75%' : '35%'"
                 ></v-img>
-                <h1 class="text-yellow-darken-3 mb-8">PawTrack</h1>
                 <p>Registration</p>
               </v-card-title>
 
-              <v-card-text class="pt-0">
+              <v-card-text>
 
-                <v-divider class="my-5"></v-divider>
+                <v-divider class="my-3"></v-divider>
 
                 <RegisterForm class="text-amber-darken-4"></RegisterForm>
 
-                <v-divider class="my-5"></v-divider>
+                <v-divider class="my-3"></v-divider>
 
                 <!-- Register Link -->
                 <h4 class="text-center">
